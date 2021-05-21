a7e3b683c679dbafdb8b77d8bf9aa661bcf14d38
kymok <kaoru.yamaoka@gmail.com>
2021-05-22 01:16:50 +0900

    Update keymap.c

--- keyboards/alpdesigns/ichii/keymaps/default/keymap.c
+++ keyboards/alpdesigns/ichii/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,_______,KC_DEL
     ),
     [_FN2] = LAYOUT(
-        _______,KC_PGUP,KC_HOME,KC_UP,  KC_END, _______,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
+        KC__MUTE,KC_PGUP,KC_HOME,KC_UP,  KC_END, _______,KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
         KC_CAPS,KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,_______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  _______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,_______
